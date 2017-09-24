8a347a002f224d599f307c0f1f0d11dac6050a18
CoderMohd <itfreelancer96@gmail.com>
2017-09-24 06:29:45 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,7 +10,7 @@ end
 def input_to_index(user_input)
 input_to_integer = user_input.to_i
 index = input_to_integer - 1
-end  
+end
 def move(board,position,character = "X")
   board[position] = character
 end  
